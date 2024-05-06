344f706430b2165ddb9c9569bc9128c2f158a22d
Manoj <mnjknh587@gmail.com>
2024-05-06 22:00:42 +0530

    4th file

---
+++ Continue_keyword.java
@@ -0,0 +1,17 @@
+package Assignments;
+
+public class Continue_keyword {
+
+	public static void main(String[] args) {
+      int a = 10;
+      for(a=0;a<=10;a++)
+      {
+    	  if(a==5)
+    	  {
+    		  continue ;
+    	  }
+    	  System.out.println(a);
+      }
+	}
+
+}
