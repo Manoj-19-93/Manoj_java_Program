d97d08b481469fdd238f4c0dadf27d53329c9b7e
Manoj <mnjknh587@gmail.com>
2024-05-06 20:45:03 +0530

    my first commit

---
+++ AllDatatypes.java
@@ -0,0 +1,20 @@
+package Assignments;
+
+public class AllDatatypes {
+	
+	byte a ; // Declaration of byte 
+	int b ;  // Declaration of integer data type
+	short c; // Declaration of short
+	long d ; // Declaration of long
+	double e; // Declaration of double
+	float f ; // Declaration of float
+	char g;   // Declaration of char
+	boolean h; // Declaration of boolean
+	
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
